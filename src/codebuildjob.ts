6893d795429c50254469a754c63802c20ac0eb04
Andrei Kazakou <a.v.kazakou@gmail.com>
2022-07-08 16:33:39 +0600

    Added first implementation of GitHub Action

---
+++ src/codebuildjob.ts
@@ -0,0 +1,92 @@
+import * as core from '@actions/core';
+import { CodeBuild } from 'aws-sdk';
+import { Logger } from './logger';
+import { BatchGetBuildsOutput, BuildPhaseType, Builds, LogsLocation, StartBuildInput } from 'aws-sdk/clients/codebuild';
+import { EventEmitter } from 'events';
+
+class CodeBuildJob extends EventEmitter {
+  protected params: StartBuildInput;
+  protected client = new CodeBuild();
+  protected build: CodeBuild.Build | undefined = undefined;
+  protected logger: Logger | undefined;
+  protected timeout: NodeJS.Timeout | undefined;
+  protected currentPhase: BuildPhaseType | 'STARTING' = 'STARTING';
+
+  constructor(params: StartBuildInput) {
+    super();
+    this.params = params;
+
+    this.wait = this.wait.bind(this);
+
+    this.on('phaseChanged', this.onPhaseChanged.bind(this));
+    this.on('COMPLETED', this.onCompleted.bind(this));
+  }
+
+  /**
+   * Starting a CodeBuild job
+   */
+  public async startBuild() {
+    const { projectName } = this.params;
+
+    core.info(`Starting "${projectName}" CodeBuild project job`);
+    const startBuildOutput = await this.client.startBuild(this.params).promise();
+
+    if (!startBuildOutput || !startBuildOutput.build) {
+      throw new Error(`Can't start ${projectName} CodeBuild job. Empty response from AWS API Endpoint`);
+    }
+
+    const { build } = startBuildOutput;
+    this.build = build;
+
+    core.info(`CodeBuild project job ${build.id} was started successfully`);
+
+    // initiate logger listening
+    const { cloudWatchLogs } = build.logs as LogsLocation;
+    if (cloudWatchLogs && cloudWatchLogs.status === 'ENABLED') {
+      const options = {
+        type: 'cloudwatch',
+        logGroupName: (cloudWatchLogs.groupName || `/aws/codebuild/${projectName}`) as string,
+        logStreamName: (cloudWatchLogs.streamName || (build.id as string).split(':').at(-1)) as string,
+      }
+
+      this.logger = new Logger(options);
+      await this.wait();
+    }
+  }
+
+  /**
+   * Wait till build job reach phase where logs can be obtained
+   * @protected
+   */
+  protected async wait() {
+    const { id } = this.build as CodeBuild.Build;
+    const { builds } = await this.client.batchGetBuilds({ ids: [ id as string ] }).promise() as BatchGetBuildsOutput;
+    const build = (builds as Builds).at(0);
+
+    if (build?.currentPhase !== this.currentPhase) {
+      this.currentPhase = build?.currentPhase as BuildPhaseType;
+      this.emit('phaseChanged', build?.currentPhase);
+    }
+
+    if (this.currentPhase !== 'COMPLETED') {
+      this.timeout = setTimeout(this.wait, 1000);
+    }
+  }
+
+  protected onPhaseChanged(phase: BuildPhaseType) {
+    core.info(`Build phase was changed to the "${this.currentPhase}" status`);
+    this.emit(phase);
+
+    if (!(['SUBMITTED', 'QUEUED', 'PROVISIONING'] as BuildPhaseType[]).includes(phase)) {
+      this.logger?.start();
+    }
+  }
+
+  protected onCompleted() {
+    this.logger?.stop();
+  }
+}
+
+export {
+  CodeBuildJob,
+}
---
+++ src/logger.ts
@@ -0,0 +1,182 @@
+import * as core from '@actions/core';
+import { CloudWatchLogs } from 'aws-sdk';
+import { EventEmitter } from 'events';
+import { GetLogEventsRequest, OutputLogEvent, OutputLogEvents, Timestamp } from 'aws-sdk/clients/cloudwatchlogs';
+
+/**
+ * CloudWatchLogs logs stream connector
+ */
+class CloudWatchLogger extends EventEmitter {
+  /**
+   * Client for connecting to the CloudWatchLogs log streams
+   * @protected
+   */
+  protected client = new CloudWatchLogs();
+
+  /**
+   * Parameters for getting log stream
+   * @protected
+   */
+  protected params: { logGroupName: string, logStreamName: string };
+
+  /**
+   * Latest processed log record timestamp
+   * @protected
+   */
+  protected maxTimestamp: Timestamp = 0;
+
+  /**
+   * Timeout before doing next request for getting logs request again
+   * @protected
+   */
+  protected timeout: NodeJS.Timeout | undefined;
+
+  /**
+   * Indicator that telling we should stop a watching logs
+   * @protected
+   */
+  protected isStopping = false;
+
+  /**
+   * Create a new instance of CloudWatchLogs stream logger listener
+   * @param { logGroupName: string, logStreamName: string } params
+   */
+  constructor(params: { logGroupName: string, logStreamName: string }) {
+    super();
+
+    this.params = params;
+    this.getEvents = this.getEvents.bind(this);
+    this.startListen = this.startListen.bind(this);
+  }
+
+  /**
+   * Start listening CloudWatch log stream
+   */
+  public async startListen() {
+    try {
+      await this.getEvents();
+    } catch (e) {
+      if ((e as Error).name === 'ResourceNotFoundException') {
+        core.info(`CloudWatch stream ${this.params.logGroupName}/${this.params.logStreamName} not found. Trying again...`);
+        return;
+      }
+
+      core.error(e as Error);
+    }
+
+    if (!this.isStopping) {
+      this.timeout = setTimeout(this.startListen, 1000);
+    }
+  }
+
+  /**
+   * Stop listening CloudWatchLogs stream
+   */
+  public stopListen() {
+    this.isStopping = true;
+    if (this.timeout) {
+      clearTimeout(this.timeout);
+    }
+  }
+
+  /**
+   * Getting events from CloudWatch log stream
+   * @param {GetLogEventsRequest=} req - Request logs options
+   * @protected
+   */
+  protected async getEvents(req?: GetLogEventsRequest) {
+    const request: GetLogEventsRequest = {...(req || this.params as GetLogEventsRequest)};
+    request.limit = 1000;
+
+    if (this.maxTimestamp) {
+      request.startTime = this.maxTimestamp;
+    }
+
+    const { events , nextForwardToken } = await this.client.getLogEvents(request).promise();
+
+    if (events && events.length > 0) {
+      // reporting about new messages in the logs stream
+      events.forEach(e => this.emit('message', e));
+
+      // if we have more than one page in stream response,
+      // doing additional requests for getting new messages
+      if (nextForwardToken) {
+        const maxTimestamp = Math.max(...(events as OutputLogEvents).map(e => e.timestamp as number));
+
+        if (maxTimestamp > this.maxTimestamp) {
+          this.maxTimestamp = maxTimestamp;
+        }
+
+        // recursively calling request for getting additional log events
+        await this.getEvents({ ...request, nextToken: nextForwardToken });
+      }
+    }
+  }
+}
+
+/**
+ * Decorator class for getting logs from different sources
+ */
+class Logger {
+  /**
+   * Connector logger instance
+   * @protected
+   */
+  protected logger: CloudWatchLogger;
+  /**
+   * Is connector instance already listening new events
+   * @protected
+   */
+  protected isStarted = false;
+
+  /**
+   * Creates a new logger instance
+   * @param { type: string, logGroupName: string, logStreamName: string } params
+   * @param {string} params.type - Type of connector that required to be launched
+   * @param {string} params.logGroupName - CloudWatch Logs group name
+   * @param {string} params.logStreamName - CloudWatch Stream name in provided CloudWatch Logs group name
+   */
+  constructor({ type, logGroupName, logStreamName }: { type: string, logGroupName: string, logStreamName: string }) {
+    if (type === 'cloudwatch') {
+      this.logger = new CloudWatchLogger({ logGroupName, logStreamName })
+    } else {
+      throw new Error(`No found CloudWatch config for listening`)
+    }
+
+    this.logger.on('message', this.listener.bind(this));
+  }
+
+  /**
+   * Begin listening logs stream
+   */
+  public start() {
+    if (!this.isStarted) {
+      this.isStarted = true;
+      this.logger.startListen();
+    }
+  }
+
+  /**
+   * Stop listening logs stream
+   */
+  public stop() {
+    this.logger.stopListen();
+
+    setTimeout(() => {
+      this.logger.removeAllListeners();
+    }, 0);
+  }
+
+  /**
+   * Listener that reacts to the new log events in logs stream
+   * @param {OutputLogEvent} e - Event that contains separate log message from log stream
+   * @protected
+   */
+  protected listener(e: OutputLogEvent) {
+    e && e.message && core.info(e.message.trim());
+  }
+}
+
+export {
+  Logger,
+}
