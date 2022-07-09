bb2e053f9208a23ee966fcf55894224e35f288d7
Andrei Kazakou <a.v.kazakou@gmail.com>
2022-07-09 15:10:46 +0600

    Implementation of verification code build jobs (#1)
    * Implementation of verification code build jobs
    
    * Change to build code locally before executing action
    
    * Fix path to dist
    
    * Fix path to dist
    
    * Added copyfiles to copying the action.yml file
    
    * Added copyfiles to copying the action.yml file
    
    * Added environment variables for AWS using

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,10 @@
 import * as core from '@actions/core';
-import * as github from '@actions/github';
+import { CodeBuildJob } from './codebuildjob'
 
 try {
-  // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Hello ${nameToGreet}!`);
-  const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
+  const projectName = core.getInput('projectName');
+  const job = new CodeBuildJob({ projectName })
+  job.startBuild().catch(error => core.setFailed(error as Error));
 } catch (error) {
   core.setFailed(error as Error);
 }
