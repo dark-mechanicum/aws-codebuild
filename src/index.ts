cb19e1650d52e849f093d36d69a0b8c3aa062df0
Andrei Kazakou <a.v.kazakou@gmail.com>
2022-07-09 16:59:41 +0600

    Add nconf as INPUT provider

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,11 @@
 import * as core from '@actions/core';
-import { CodeBuildJob } from './codebuildjob'
+import { CodeBuildJob } from './codebuildjob';
+import * as nconf from 'nconf';
 
-try {
-  const projectName = core.getInput('projectName');
-  const job = new CodeBuildJob({ projectName })
-  job.startBuild().catch(error => core.setFailed(error as Error));
-} catch (error) {
-  core.setFailed(error as Error);
-}
+const config = nconf.env({ separator: '_', parseValues: true }).overrides();
+
+const job = new CodeBuildJob({
+  projectName: config.get('INPUT:projectName'),
+});
+
+job.startBuild().catch(error => core.setFailed(error as Error));
