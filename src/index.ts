ddb06ec85ab56ce1659d1d488bf74180713f290f
Andrei Kazakou <a.v.kazakou@gmail.com>
2022-07-09 17:24:16 +0600

    Using only ENV variables

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,14 @@ import * as core from '@actions/core';
 import nconf from 'nconf';
 import { CodeBuildJob } from './codebuildjob';
 
-const config = nconf.env({ separator: '_', parseValues: true });
+const config = nconf.env({
+  separator: '__',
+  parseValues: true,
+  match: /^CODEBUILD__/,
+});
 
 const job = new CodeBuildJob({
-  projectName: config.get('INPUT:projectName'),
+  ...config.get('CODEBUILD'),
 });
 
 job.startBuild().catch(error => core.setFailed(error as Error));
